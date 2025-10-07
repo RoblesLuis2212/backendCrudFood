5f624dc7f8410dad7fa0c17763fb025a0f531076
Luis Robles <luisgeremiasrobles@gmail.com>
2025-10-07 20:56:28 -0300

    Agregamos una ruta post de prueba

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -2,3 +2,7 @@ export const prueba = (req, res) => {
   console.log("Desde el controlador de prueba");
   res.send("Prueba desde el controlador");
 };
+
+export const crearProducto = (req, res) => {
+  res.send("Aqui crearemos el producto");
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { prueba } from "../controllers/productos.controllers.js";
+import { crearProducto, prueba } from "../controllers/productos.controllers.js";
 /*GET - POST - PATH O PUT - DELETE */
 
 const router = Router();
 
 router.route("/test").get(prueba);
+router.route("/").post(crearProducto);
 
 export default router;
