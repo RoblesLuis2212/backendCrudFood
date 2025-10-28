2807ed867896f9f5c761cc4cc2fe14559361f334
Luis Robles <luisgeremiasrobles@gmail.com>
2025-10-28 20:22:38 -0300

    agregamos validaciones para los campos descripcion breve y amplia y tambien para el campo de la imagen utilizando una expresion regular

--- src/middlewares/validacionProducto.js
+++ src/middlewares/validacionProducto.js
@@ -17,6 +17,46 @@ const validacionProducto = [
     .withMessage(
       "El precio debe estar entre 100 y 1000000 de pesos argentinos"
     ),
+  body("descripcion_breve")
+    .notEmpty()
+    .withMessage("la descripcion breve es un dato obligatorio")
+    .isLength({ min: 5, max: 250 })
+    .withMessage("La descripcion breve debe tener entre 5 y 100 caracteres"),
+  body("descripcion_amplia")
+    .notEmpty()
+    .withMessage("la descripcion amplia es un dato obligatorio")
+    .isLength({ min: 10, max: 500 })
+    .withMessage("La descripcion amplia debe tener entre 10 y 500 caracteres"),
+  body("categoria")
+    .notEmpty()
+    .withMessage("La categoria es un dato obligatorio")
+    .isIn([
+      "Acompañamientos",
+      "Bebidas",
+      "Ensaladas",
+      "Hamburguesas",
+      "Postres",
+      "Pizzas",
+      "Sandwiches y Wraps",
+      "Veganos",
+    ])
+    .withMessage(`La categoria debe ser una de las siguiente: "Acompañamientos"
+      "Bebidas",
+      "Ensaladas",
+      "Hamburguesas",
+      "Postres",
+      "Pizzas",
+      "Sandwiches y Wraps",
+      "Veganos"`),
+  body("imagen")
+    .notEmpty()
+    .withMessage("La imagen es un dato obligatorio")
+    .matches(
+      /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?(\.(jpg|jpeg|png|webp))$/
+    )
+    .withMessage(
+      "La imagen debe cumplir con el formato de url de imagen con los siguientes formatos: jpg, jpeg, png, webp"
+    ),
   (req, res, next) => resultadoValidacion(req, res, next),
 ];
 
