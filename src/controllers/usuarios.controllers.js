eec0aad6ed313761518a2a74208d7142b4000880
Luis Robles <luisgeremiasrobles@gmail.com>
2025-10-30 21:01:03 -0300

    Hasheamos el password de el usuario al momento de crearlo y agregamos listar usuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,7 +1,11 @@
 import Usuario from "../models/usuario.js";
+import bcrypt from "bcrypt";
 
 export const crearUsuario = async (req, res) => {
   try {
+    const saltos = bcrypt.genSaltSync(10);
+    const passwordEncriptado = bcrypt.hashSync(req.body.password, saltos);
+    req.body.password = passwordEncriptado;
     const usuarioNuevo = new Usuario(req.body);
     await usuarioNuevo.save();
     res.status(201).json({ mensaje: "usuario creado correctamente" });
@@ -10,5 +14,17 @@ export const crearUsuario = async (req, res) => {
     res
       .status(500)
       .json({ mensaje: "Ocurrio un error no se pudo crear el usuario" });
+  }
+};
+
+export const listarUsuarios = async (req, res) => {
+  try {
+    const usuarios = await Usuario.find({});
+    res.status(200).json(usuarios);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error no se pudo listar los usuarios" });
   }
 };
